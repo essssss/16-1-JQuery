5604865acbe933c00aaf063c1fb6d6524d1a7a5a
essssss <srmcnamara7@gmail.com>
2023-03-04 13:03:54 -0500

    done done

---
+++ Demo-Code/app2.js
@@ -0,0 +1,19 @@
+// JQUERY
+// First thing we do is the $ sign. 
+// $ acts like "querySelectorAll"
+// $ is an alias for the jQuery() function.
+
+// We don't get a node List. we get a jQuery object!
+
+// NOT the same as DOM elements
+
+// We can use ".get" to get an element or array.
+
+// ************************************************** //
+// jQuery getter/setter methods.
+
+// ".text()" allows us to get the inner text. But ALSO, we can change it , with one swoop
+
+// With jQuery we can frequently updat multiple elements at once. Without looping.
+
+
---
+++ exercise/app.js
@@ -0,0 +1,23 @@
+$(function () {
+   console.log("Let's party with JQUERY");
+
+   $('article img').addClass('image-center');
+   $('article p:last-child').remove();
+   $('title').css('font-size', Math.random() * 100);
+   $('ol').append($('<li>', { text: 'whatever you want' }));
+   $('aside')
+      .empty()
+      .append($('<p>', { text: 'no more lists' }));
+   $('.form-control').on('keyup blur change', function () {
+      let red = $('.form-control').eq(0).val();
+      let blue = $('.form-control').eq(1).val();
+      let green = $('.form-control').eq(2).val();
+      $('body').css(
+         'background-color',
+         'rgb(' + red + ',' + green + ',' + blue + ')'
+      );
+   });
+   $('img').on('click', function (e) {
+      $(e.target).remove();
+   });
+});
